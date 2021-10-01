89c47723f529b61e7cd067aa2fc871d5f58aad25
이성현 <74307591+kiki3700@users.noreply.github.com>
2021-10-02 00:06:38 +0900

    util and move to mysql

--- src/main/java/com/example/demo/util/formatConverter.java
+++ src/main/java/com/example/demo/util/formatConverter.java
@@ -5,10 +5,14 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateConverter {
+public class formatConverter {
 	public Date StringToDate(String dateString) throws ParseException {
 	SimpleDateFormat format = new SimpleDateFormat("yyyMMdd");
 	Date date = format.parse(dateString);
 	return date;
+	}
+	public String dateToString(Date date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyMMdd");
+		return format.format(date);
 	}
 }
