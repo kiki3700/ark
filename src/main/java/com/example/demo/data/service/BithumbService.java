4474c8d65fd22fafd37aebd4c8afa93caed53bf8
이성현 <5575492q@gmail.com>
2021-10-13 14:24:38 +0900

    bithumservice

---
+++ src/main/java/com/example/demo/data/service/BithumbService.java
@@ -0,0 +1,9 @@
+package com.example.demo.data.service;
+
+import java.util.Map;
+
+public interface BithumbService {
+
+	void getCrytoCurrencyHistory(Map<String, Object> inParams);
+
+}
--- src/main/java/com/example/demo/data/service/impl/BithumbServiceImpl.java
+++ src/main/java/com/example/demo/data/service/impl/BithumbServiceImpl.java
@@ -11,28 +11,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.demo.data.service.BithumbService;
 import com.example.demo.vo.HistoryDataDto;
 
 @Service
-public class BithumbServiceImpl {
+public class BithumbServiceImpl implements BithumbService{
 	@Value("${bithumb.url")
 	String url;
 	
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Override
 	public void getCrytoCurrencyHistory(Map<String, Object>inParams) {
-		List<HistoryDataDto> resultList = new LinkedList<HistoryDataDto>();
 		String targetUrl = url+"/public/candlestick/{nm}_KRW/24h";
-		Map<String, String> map = new HashMap<>();
-		map.put("nm",(String) inParams.getOrDefault("name", null));
-		HashMap<String, Object> result = restTemplate.getForObject(targetUrl, HashMap.class);
+		List<HistoryDataDto> resultList = new LinkedList<HistoryDataDto>();
+		Map<String, String> queryMap = new HashMap<>();
+		queryMap.put("nm",(String) inParams.getOrDefault("name", null));
+		int quant = (int) inParams.getOrDefault("quant", 0);
+		HashMap<String, Object> result = restTemplate.getForObject(targetUrl, HashMap.class, queryMap);
 		
 		String msg =(String) result.get("status");
 		if(msg.equals("0000")) {
 			List<Object[]> datas = (List<Object[]>) result.get("data");
 			int len = datas.size();
-			for(int i = 0; i < len; i++) {
+			for(int i = len-quant; i < len; i++) {
 				HistoryDataDto historyDataDto = new HistoryDataDto();
 				Object[] data = datas.get(i);
 				historyDataDto.setTradingDate(new Date(Long.parseLong((String) data[0])));
